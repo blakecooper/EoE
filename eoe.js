5777b57cd3ed610f880e435193f258423beacaeb
Blake <contact@blake-cooper.com>
2018-11-15 16:51:06 -0500

    Added photo functionality

--- eoe.js
+++ eoe.js
@@ -4,7 +4,8 @@ let substrings = [
   `<div class="introtext text parbase section"><p>`,
   `srcset=`,
   `srcset=`,
-  `<div class="calltoaction section">`
+  `<div class="calltoaction section">`,
+  `src="/content/shared`
 ]
 
 //EoE elements to populate from page source
@@ -20,26 +21,34 @@ function $(id) {
     return document.getElementById(id)
     }
 
-//Search page source to populate the elements correctly
+//Search page source to populate the elements (almost) correctly
 function getElements() {
-  for(let i = 0; i < 5; i++) {
+  for(let i = 0; i < 6; i++) {
     var idxBeg = $("source").value.indexOf(substrings[i]);
     eoeElements[i] = $("source").value.substring(idxBeg + substrings[i].length - 1);
     var idxEnd = eoeElements[i].indexOf("</");
     eoeElements[i] = eoeElements[i].substring(i,idxEnd);
   }
 
-  //A little cleanup once the loop has run to catch irregularities
+  //A little post-loop cleanup to catch irregularities
   eoeElements[0] = eoeElements[0].substring(1,eoeElements[0].length)
   startOfLinkText = eoeElements[4].indexOf(`>`) + 1
   eoeElements[4] = eoeElements[4].substring(startOfLinkText,eoeElements[4].length)
   endOfLink = eoeElements[3].indexOf('jcr:') -1
   eoeElements[3] = "http://www.buffalo.edu/ubit/news/article.host.html/" + eoeElements[3].substring(0, endOfLink) + ".detail.html"
+  endOfImageLink = eoeElements[5].indexOf(`"`)
+  eoeElements[5] = "http://www.buffalo.edu/content/shared/www" + eoeElements[5].substring(0,endOfImageLink)
 
-  document.getElementById('textArea').innerHTML = "<b>Title: </b>" + eoeElements[0] +
+  $("textArea").innerHTML = "<b>Title: </b>" + eoeElements[0] +
   "<br><b>Teaser: </b>" + eoeElements[1] +
   "<br><b>Link text: </b>" + eoeElements[4] +
-  "<br><b>Link : </b>" + eoeElements[3];
+  "<br><b>Link: </b>" + eoeElements[3];
 
-  //TODO: get photo and set up onClick functions for buttons
+  $("image").innerHTML = "<img src='" + eoeElements[5] + "'></img>"
+
+  function copyAndImageSearch() {
+      eoeElements[5].select()
+      document.execCommand("copy")
+      window.open(eoeElements[5],"_self")
+  }
 }
